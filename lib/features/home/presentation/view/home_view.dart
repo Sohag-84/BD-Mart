ce855121087631d2e1849eb89607316dc5bc5eae
Sohag-84 <ihsohag84@gmail.com>
2025-03-30 14:01:18 +0600

    add refresher on home view

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -4,6 +4,7 @@ import 'package:gym_swat/features/home/presentation/bloc/home_bloc.dart';
 import 'package:gym_swat/features/home/presentation/part/all_product_section.dart';
 import 'package:gym_swat/features/home/presentation/part/category_section.dart';
 import 'package:gym_swat/features/home/presentation/part/home_seach_bar.dart';
+import 'package:gym_swat/features/product/presentation/bloc/product_bloc.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -40,52 +41,60 @@ class _HomeViewState extends State<HomeView> {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            BlocBuilder<HomeBloc, HomeState>(
-              buildWhen: (previous, current) =>
-                  current is HomeSliderLoaded ||
-                  current is HomeSliderLoading ||
-                  current is HomeSliderLoadingError,
-              builder: (context, state) {
-                if (state is HomeSliderLoading) {
-                  return SizedBox(
-                    height: 160.h,
-                    child: Center(child: loader()),
-                  );
-                } else if (state is HomeSliderLoadingError) {
-                  return Center(
-                    child: Text(state.message),
-                  );
-                } else if (state is HomeSliderLoaded) {
-                  return homeSlider(
-                    imageList:
-                        state.sliderList.map((slider) => slider.photo).toList(),
-                    boxFit: BoxFit.fill,
-                  );
-                } else {
-                  return const SizedBox();
-                }
-              },
-            ),
-            const Gap(10),
-
-            ///others section
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 12.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ///categories
-                  const CategorySection(),
-                  Gap(15.h),
-
-                  allProductsSection(context: context),
-                ],
+      body: refresh(
+        onRefresh: () {
+          context.read<HomeBloc>().add(HomeSliderFetchedEvent());
+          context.read<HomeBloc>().add(HomeCategoryFetchedEvent());
+          context.read<ProductBloc>().add(ProductFetchedEvent());
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              BlocBuilder<HomeBloc, HomeState>(
+                buildWhen: (previous, current) =>
+                    current is HomeSliderLoaded ||
+                    current is HomeSliderLoading ||
+                    current is HomeSliderLoadingError,
+                builder: (context, state) {
+                  if (state is HomeSliderLoading) {
+                    return SizedBox(
+                      height: 160.h,
+                      child: Center(child: loader()),
+                    );
+                  } else if (state is HomeSliderLoadingError) {
+                    return Center(
+                      child: Text(state.message),
+                    );
+                  } else if (state is HomeSliderLoaded) {
+                    return homeSlider(
+                      imageList: state.sliderList
+                          .map((slider) => slider.photo)
+                          .toList(),
+                      boxFit: BoxFit.fill,
+                    );
+                  } else {
+                    return const SizedBox();
+                  }
+                },
               ),
-            ),
-          ],
+              const Gap(10),
+
+              ///others section
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ///categories
+                    const CategorySection(),
+                    Gap(15.h),
+
+                    allProductsSection(context: context),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
