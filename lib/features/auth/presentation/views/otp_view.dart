ae361c313bdac7e9cbd1438b144db345f4c3b77f
Sohag-84 <ihsohag84@gmail.com>
2025-08-02 17:09:42 +0600

    designed otp view

--- lib/features/auth/presentation/views/otp_view.dart
+++ lib/features/auth/presentation/views/otp_view.dart
@@ -1,4 +1,6 @@
 import 'package:gym_swat/core/constants/exports.dart';
+import 'package:gym_swat/core/widgets/custom_button.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpView extends StatefulWidget {
   final String emailOrPhone;
@@ -14,11 +16,65 @@ class OtpView extends StatefulWidget {
 }
 
 class _OtpViewState extends State<OtpView> {
+  final otpController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Otp"),
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              const Text(
+                "OTP Verification",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "We sent your code to ${widget.emailOrPhone}",
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Color(0xFF757575)),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+
+              ///otp textfield
+              Pinput(
+                length: 6,
+                controller: otpController,
+                pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+              ),
+              const SizedBox(height: 24),
+
+              ///otp submit button
+              customButton(
+                onTap: () {},
+                btnText: "Continue",
+                fontColor: AppColors.whiteColor,
+                height: 45,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+
+              ///otp resend button
+              TextButton(
+                onPressed: () async {},
+                child: const Text(
+                  "Resend OTP Code",
+                  style: TextStyle(color: Color(0xFF757575)),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
